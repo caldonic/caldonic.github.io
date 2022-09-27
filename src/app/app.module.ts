b0419e2f3567082d164341edee26e050e00365c9
SYSTEX\2200779 <caldonic@gmail.com>
2022-09-27 15:10:14 +0800

    full code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
+import { CartComponent } from './cart/cart.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    AppRoutingModule,
+    FormsModule,
+    BooksModule,
+    AuthModule
+    ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
